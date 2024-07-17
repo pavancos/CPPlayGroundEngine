47a4fb34d16907e703d4e664ba3efc1621b9842c
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-07-17 20:17:05 +0530

    chore: Update puppeteer dependency to include puppeteer-core

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport:false,
+        userDataDir: './tmp',
+    });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('https://www.amazon.in/s?k=Macbook');
     await page.screenshot({ path: 'example.png' });
+    // Wait for 10 seconds
+    await new Promise(resolve => setTimeout(resolve, 5000));
     await browser.close();
     }
 )();
