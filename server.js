82ffb5ddfd82ae9bb6bae0d75c3f4f1fc4ff9b64
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-08-31 12:15:53 +0530

    chore: changed port to env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 
 // Scrape AtCoder data
